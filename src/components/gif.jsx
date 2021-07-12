d134fc98f6ee3117f45f2adfd7a40753d7336557
Bob Dijks <rhcdijks@gmail.com>
2021-07-12 18:38:21 -0500

    added an empty key test

--- src/components/gif.jsx
+++ src/components/gif.jsx
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 
 class Gif extends Component {
   render() {
+      // Testing
+      if (!this.props.id) {
+        return null;
+      }
+
       const src = `https://i.giphy.com/${this.props.id}/200.gif`;
       return (
       <img src={src} alt="" className="gif" />
